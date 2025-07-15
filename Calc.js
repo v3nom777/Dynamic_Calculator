7cc4f2707086fb88ec33fee551e57d7dfc184c1f
Anurag_Sarkar <147474527+v3nom777@users.noreply.github.com>
2025-07-16 00:03:48 +0530

    Update Calc.js

--- Calc.js
+++ Calc.js
@@ -1,4 +1,20 @@
 function dis(n){
     console.log(`Button ${n} pressed`)  
-    document.getElementById("result").value += n
+    document.getElementById("result").innerHTML+= n
+}
+function solv(){
+    try{
+        var s= document.getElementById("result").innerHTML;
+        console.log(s);
+        document.getElementById("result").innerHTML = eval(s)
+        console.log(`Button ${n} pressed`)  
+    
+    }
+    catch (error){
+ alert("wrong information format");
+    }
+   
+}
+function clr(){
+    document.getElementById("result").innerHTML = "";
 }
